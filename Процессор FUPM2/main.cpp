55c3fa46586ec6edd208aa497741f03748ac6110
Kyzyl-ool <kyzyloolk@mail.ru>
2018-05-08 19:36:35 +0300

    Initial Commit

---
+++ Процессор FUPM2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Процессор FUPM2
+//
+//  Created by Кежик Кызыл-оол on 08.05.2018.
+//  Copyright © 2018 Кежик Кызыл-оол. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
